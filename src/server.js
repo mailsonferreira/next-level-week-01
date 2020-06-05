5ba89e81e8a48435f1aef8c674241c856a1ecbb4
Mailson Ferreira <60264180+mailsonferreira@users.noreply.github.com>
2020-06-05 20:23:49 -0300

    add db e ajustes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const server = express()
 
+//pegar o banco de dados
+ const db = require("./database/db")
 
 // Configurar pasta publica
 server.use(express.static("public"))
@@ -8,10 +10,10 @@ server.use(express.static("public"))
 
 //utilizando templante engine
 const nunjucks = require("nunjucks")
-    nunjucks.configure("src/views", {
-        express: server,
-        noCache: true
-    })
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true
+})
 
 
 //configurar caminhos da aplicação
@@ -19,16 +21,29 @@ const nunjucks = require("nunjucks")
 // req: Requisição
 // res: Resposta
 server.get("/", (req, res) => {
-  return  res.render("index.html", { title: "Um titulo" })
+  return res.render("index.html", { title: "Um titulo" })
 })
 
 server.get("/create-point", (req, res) => {
-  return  res.render("create-point.html")
+  return res.render("create-point.html")
 })
 
 server.get("/search", (req, res) => {
-    return  res.render("search-results.html")
+
+  //PEGAR OS DADOS DO BANCO DE DADOS
+  db.all(`SELECT * FROM places`, function (err, rows) {
+    if (err) {
+      return console.log(err)
+    }
+
+    const total = rows.length
+
+    //mostrar a pagina html com os dados do banco de dados 
+    return res.render("search-results.html", { places: rows, total: total })
+
   })
+
+})
 
 
 
